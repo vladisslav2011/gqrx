bd06de2eeacfd3b1c5eb61c65264a87afbd607db
Vladisslav P <vladisslav@inbox.ru>
2021-12-16 01:44:32 +0300

    IQ Tool GUI improvements
    Fix jumpy scrollbar of IQ player
    
    When file list grows long and the user tries to scroll the list to a new
    postion, the list continues to return it's scroll to the last selected item.
    That's really annoying.
    Return the list to last seen position after refreshing it's contents.
    
    IQ recorder: disable harmful buttons while recording is in progress.
    
    Changing IO devices and loading/saving settings does not look like good thing
    to do while recording an IQ file.
    
    IQ tool: Always choose correct sampling rate
    
    Reselect file before starting playback. Fixes incorrect sample rate
    when playback is started, stopped, devices switched, dsp started,
    stopped and then started playback of the same IQ file.
    
    IQ tool: disable/enable controls properly
    
    Disable directory selector, file list while playing/recording  IQ
    file.
    Disable slider while recording IQ file.

--- src/applications/gqrx/mainwindow.cpp
+++ src/applications/gqrx/mainwindow.cpp
@@ -1593,6 +1593,8 @@ void MainWindow::startIqRecording(const QString& recdir)
             toString("%1/gqrx_yyyyMMdd_hhmmss_%2_%3_fc.'raw'")
             .arg(recdir).arg(freq).arg(sr/dec);
 
+    ui->actionIoConfig->setDisabled(true);
+    ui->actionLoadSettings->setDisabled(true);
     // start recorder; fails if recording already in progress
     if (rx->start_iq_recording(lastRec.toStdString()))
     {
@@ -1623,6 +1625,8 @@ void MainWindow::stopIqRecording()
         ui->statusBar->showMessage(tr("Error stopping I/Q recoder"));
     else
         ui->statusBar->showMessage(tr("I/Q data recoding stopped"), 5000);
+    ui->actionIoConfig->setDisabled(false);
+    ui->actionLoadSettings->setDisabled(false);
 }
 
 void MainWindow::startIqPlayback(const QString& filename, float samprate, qint64 center_freq)
@@ -1634,6 +1638,8 @@ void MainWindow::startIqPlayback(const QString& filename, float samprate, qint64
     }
 
     storeSession();
+    backupFreq = ui->freqCtrl->getFrequency();
+    backupOffset = (qint64) rx->get_filter_offset();
 
     auto sri = (int)samprate;
     auto cf  = center_freq;
@@ -1665,6 +1671,9 @@ void MainWindow::startIqPlayback(const QString& filename, float samprate, qint64
 
     // FIXME: would be nice with good/bad status
     ui->statusBar->showMessage(tr("Playing %1").arg(filename));
+    ui->actionIoConfig->setDisabled(true);
+    ui->actionLoadSettings->setDisabled(true);
+    ui->actionSaveSettings->setDisabled(true);
 
     on_actionDSP_triggered(true);
 }
@@ -1678,6 +1687,9 @@ void MainWindow::stopIqPlayback()
     }
 
     ui->statusBar->showMessage(tr("I/Q playback stopped"), 5000);
+    ui->actionIoConfig->setDisabled(false);
+    ui->actionLoadSettings->setDisabled(false);
+    ui->actionSaveSettings->setDisabled(false);
 
     // restore original input device
     auto indev = m_settings->value("input/device", "").toString();
