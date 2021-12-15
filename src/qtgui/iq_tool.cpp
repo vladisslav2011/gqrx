751edd1808ddc0d2ab39d98e5e98704069db5fd5
Vladisslav P <vladisslav@inbox.ru>
2021-12-16 01:44:32 +0300

    IQ Tool GUI improvements
    Fix jumpy scrollbar of IQ player
    
    When file list grows long and the user tries to scroll the list to a new
    postion, the list continues to return it's scroll to the last selected item.
    That's really annoying.
    Return the list to last seen position after refreshing it's contents.
    
    IQ recorder: disable harmful buttons while recording is in progress.
    
    Changing IO devices and loading/saving settings does not look like good thing
    to do while recording an IQ file.
    
    IQ tool: Always choose correct sampling rate
    
    Reselect file before starting playback. Fixes incorrect sample rate
    when playback is started, stopped, devices switched, dsp started,
    stopped and then started playback of the same IQ file.
    
    IQ tool: disable/enable controls properly
    
    Disable directory selector, file list while playing/recording  IQ
    file.
    Disable slider while recording IQ file.

--- src/qtgui/iq_tool.cpp
+++ src/qtgui/iq_tool.cpp
@@ -29,6 +29,7 @@
 #include <QString>
 #include <QStringList>
 #include <QTime>
+#include <QScrollBar>
 
 #include <math.h>
 
@@ -100,6 +101,20 @@ void CIqTool::on_listWidget_currentTextChanged(const QString &currentText)
 
 }
 
+/*! \brief Show/hide/enable/disable GUI controls */
+
+void CIqTool::switchControlsState(bool recording, bool playback)
+{
+    ui->recButton->setEnabled(!playback);
+
+    ui->playButton->setEnabled(!recording);
+    ui->slider->setEnabled(!recording);
+
+    ui->listWidget->setEnabled(!(recording || playback));
+    ui->recDirEdit->setEnabled(!(recording || playback));
+    ui->recDirButton->setEnabled(!(recording || playback));
+}
+
 /*! \brief Start/stop playback */
 void CIqTool::on_playButton_clicked(bool checked)
 {
@@ -125,8 +140,9 @@ void CIqTool::on_playButton_clicked(bool checked)
         }
         else
         {
-            ui->listWidget->setEnabled(false);
-            ui->recButton->setEnabled(false);
+            on_listWidget_currentTextChanged(current_file);
+            switchControlsState(false, true);
+
             emit startPlayback(recdir->absoluteFilePath(current_file),
                                (float)sample_rate, center_freq);
         }
@@ -134,8 +150,7 @@ void CIqTool::on_playButton_clicked(bool checked)
     else
     {
         emit stopPlayback();
-        ui->listWidget->setEnabled(true);
-        ui->recButton->setEnabled(true);
+        switchControlsState(false, false);
         ui->slider->setValue(0);
     }
 }
@@ -148,9 +163,7 @@ void CIqTool::on_playButton_clicked(bool checked)
  */
 void CIqTool::cancelPlayback()
 {
-    ui->playButton->setChecked(false);
-    ui->listWidget->setEnabled(true);
-    ui->recButton->setEnabled(true);
+    switchControlsState(false, false);
     is_playing = false;
 }
 
@@ -172,7 +185,7 @@ void CIqTool::on_recButton_clicked(bool checked)
 
     if (checked)
     {
-        ui->playButton->setEnabled(false);
+        switchControlsState(true, false);
         emit startRecording(recdir->path());
 
         refreshDir();
@@ -180,7 +193,7 @@ void CIqTool::on_recButton_clicked(bool checked)
     }
     else
     {
-        ui->playButton->setEnabled(true);
+        switchControlsState(false, false);
         emit stopRecording();
     }
 }
@@ -303,6 +316,8 @@ void CIqTool::refreshDir()
 {
     int selection = ui->listWidget->currentRow();
 
+    QScrollBar * sc = ui->listWidget->verticalScrollBar();
+    int lastScroll = sc->sliderPosition();
     recdir->refresh();
     QStringList files = recdir->entryList();
 
@@ -310,6 +325,7 @@ void CIqTool::refreshDir()
     ui->listWidget->clear();
     ui->listWidget->insertItems(0, files);
     ui->listWidget->setCurrentRow(selection);
+    sc->setSliderPosition(lastScroll);
     ui->listWidget->blockSignals(false);
 
     if (is_recording)
