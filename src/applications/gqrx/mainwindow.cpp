ac2c01aa7c077c65557959d5074b729581948129
Vladisslav P <vladisslav@inbox.ru>
2022-02-18 22:26:46 +0300

    iq_tool: implement repeated playback option

--- src/applications/gqrx/mainwindow.cpp
+++ src/applications/gqrx/mainwindow.cpp
@@ -287,7 +287,7 @@ MainWindow::MainWindow(const QString& cfgfile, bool edit_conf, QWidget *parent) 
     // I/Q playback
     connect(iq_tool, SIGNAL(startRecording(QString, file_formats)), this, SLOT(startIqRecording(QString, file_formats)));
     connect(iq_tool, SIGNAL(stopRecording()), this, SLOT(stopIqRecording()));
-    connect(iq_tool, SIGNAL(startPlayback(QString, float, qint64, file_formats)), this, SLOT(startIqPlayback(QString, float, qint64, file_formats)));
+    connect(iq_tool, SIGNAL(startPlayback(QString, float, qint64, file_formats, bool)), this, SLOT(startIqPlayback(QString, float, qint64, file_formats, bool)));
     connect(iq_tool, SIGNAL(stopPlayback()), this, SLOT(stopIqPlayback()));
     connect(iq_tool, SIGNAL(seek(qint64)), this,SLOT(seekIqFile(qint64)));
 
@@ -1633,7 +1633,9 @@ void MainWindow::stopIqRecording()
     ui->actionLoadSettings->setDisabled(false);
 }
 
-void MainWindow::startIqPlayback(const QString& filename, float samprate, qint64 center_freq, file_formats fmt)
+void MainWindow::startIqPlayback(const QString& filename, float samprate,
+                                 qint64 center_freq,
+                                 file_formats fmt, bool repeat)
 {
     if (ui->actionDSP->isChecked())
     {
@@ -1654,7 +1656,7 @@ void MainWindow::startIqPlayback(const QString& filename, float samprate, qint64
 
     rx->set_input_device(devstr.toStdString());
     updateHWFrequencyRange(false);
-    rx->set_input_file(filename.toStdString(), samprate, fmt);
+    rx->set_input_file(filename.toStdString(), samprate, fmt, repeat);
 
     // sample rate
     auto actual_rate = rx->set_input_rate(samprate);
