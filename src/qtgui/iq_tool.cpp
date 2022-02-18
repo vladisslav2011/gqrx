50a4b2328f86a86fe5536db5de5977e1b8c70529
Vladisslav P <vladisslav@inbox.ru>
2022-02-18 22:26:46 +0300

    iq_tool: implement repeated playback option

--- src/qtgui/iq_tool.cpp
+++ src/qtgui/iq_tool.cpp
@@ -124,6 +124,7 @@ void CIqTool::switchControlsState(bool recording, bool playback)
     ui->recDirEdit->setEnabled(!(recording || playback));
     ui->recDirButton->setEnabled(!(recording || playback));
     ui->formatCombo->setEnabled(!(recording || playback));
+    ui->repeat->setEnabled(!(recording || playback));
 }
 
 /*! \brief Start/stop playback */
@@ -155,7 +156,8 @@ void CIqTool::on_playButton_clicked(bool checked)
             switchControlsState(false, true);
 
             emit startPlayback(recdir->absoluteFilePath(current_file),
-                               (float)sample_rate, center_freq, fmt);
+                               (float)sample_rate, center_freq, fmt,
+                               ui->repeat->checkState() == Qt::Checked);
         }
     }
     else
