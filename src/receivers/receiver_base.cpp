faa46d50bb51f723c11d6a0812e257c041f95bef
Vladislav P <vladisslav2011@gmail.com>
2022-08-31 02:31:28 +0300

    fix crash

--- src/receivers/receiver_base.cpp
+++ src/receivers/receiver_base.cpp
@@ -30,7 +30,7 @@
 
 static const int MIN_IN = 1;  /* Minimum number of input streams. */
 static const int MAX_IN = 1;  /* Maximum number of input streams. */
-static const int MIN_OUT = 0; /* Minimum number of output streams. */
+static const int MIN_OUT = 2; /* Minimum number of output streams. */
 static const int MAX_OUT = 2; /* Maximum number of output streams. */
 
 receiver_base_cf::receiver_base_cf(std::string src_name, float pref_quad_rate, double quad_rate, int audio_rate)
