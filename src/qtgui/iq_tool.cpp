a2c08ffc6b98e73afec31f0c9491aeb25f7e7f58
Vladslav P <vladisslav2011@gmail.com>
2022-09-22 00:07:06 +0300

    WF redraw: Fix stuck IQ tool slider in playback pause

--- src/qtgui/iq_tool.cpp
+++ src/qtgui/iq_tool.cpp
@@ -211,6 +211,7 @@ void CIqTool::on_slider_valueChanged(int value)
 
     qint64 seek_pos = (qint64)(value)*sample_rate;
     emit seek(seek_pos);
+    o_fileSize = seek_pos * bytes_per_sample;
 }
 
 
