ecd0b0739e263f0d07c8fcf01e177b9b853d39ef
Vladislav P <vladisslav2011@gmail.com>
2022-09-29 12:15:20 +0300

    wf_redraw: seek and redraw on waterfall middle-drag event

--- src/qtgui/plotter.h
+++ src/qtgui/plotter.h
@@ -125,6 +125,11 @@ public:
         m_FftCenter = qBound(-limit, f, limit);
     }
 
+    void setPlayingIQ(bool state)
+    {
+        m_PlayingIQ = state;
+    }
+
     int     getNearestPeak(QPoint pt);
     void    setWaterfallSpan(quint64 span_ms);
     quint64 getWfTimeRes() const;
@@ -138,9 +143,9 @@ public:
     void    getLockedVfos(std::vector<vfo::sptr> &to);
 
 signals:
-    void newDemodFreq(qint64 freq, qint64 delta, qint64 ts = 0); /* delta is the offset from the center */
-    void newDemodFreqLoad(qint64 freq, qint64 delta, qint64 ts = 0);/* tune and load demodulator settings */
-    void newDemodFreqAdd(qint64 freq, qint64 delta, qint64 ts = 0);/* new demodulator here */
+    void newDemodFreq(qint64 freq, qint64 delta); /* delta is the offset from the center */
+    void newDemodFreqLoad(qint64 freq, qint64 delta);/* tune and load demodulator settings */
+    void newDemodFreqAdd(qint64 freq, qint64 delta);/* new demodulator here */
     void newLowCutFreq(int f);
     void newHighCutFreq(int f);
     void newFilterFreq(int low, int high);  /* substitute for NewLow / NewHigh */
@@ -149,6 +154,8 @@ signals:
     void newFftCenterFreq(qint64 f);
     void newSize();
     void selectVfo(int);
+    void setPlaying(bool);
+    void seekIQ(qint64);
 
 public slots:
     // zoom functions
@@ -198,7 +205,8 @@ private:
         RIGHT,
         YAXIS,
         XAXIS,
-        TAG
+        TAG,
+        WATERFALL
     };
     struct wfLineStats
     {
@@ -327,6 +335,8 @@ private:
     vfo::sptr   m_lookup_vfo;
     int         m_currentVfo;
     int         m_capturedVfo;
+    bool        m_PlayingIQ;
+    qint64      m_CapturedTs;
 
     // Waterfall averaging
     quint64     tlast_wf_ms;        // last time waterfall has been updated
