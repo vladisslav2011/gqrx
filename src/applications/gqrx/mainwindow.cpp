9ba2c04cd8774e121c7130142cc273eece0c3a7b
Vladislav P <vladisslav2011@gmail.com>
2023-03-11 18:24:14 +0300

    WF redraw: redraw the panadapter too
    Redraw the panadapter
    Trigger redraw on panadapter color/fill change
    Fix waterfall middle-button dragging when the panadapter size is 0

--- src/applications/gqrx/mainwindow.cpp
+++ src/applications/gqrx/mainwindow.cpp
@@ -2686,7 +2686,7 @@ void MainWindow::waterfall_background_func()
         }
         if(background_request == MainWindow::WF_RUNNING)
         {
-            if(k<lines)
+            if(k<=lines)
             {
                 if(line<=maxlines)
                 {
@@ -2904,6 +2904,7 @@ void MainWindow::setFftColor(const QColor& color)
     ui->plotter->setFftPlotColor(color);
     uiDockAudio->setFftColor(color);
     uiDockProbe->setFftColor(color);
+    triggerIQFftRedraw();
 }
 
 /** Enable/disable filling the aread below the FFT plot. */
@@ -2912,6 +2913,7 @@ void MainWindow::setFftFill(bool enable)
     ui->plotter->setFftFill(enable);
     uiDockAudio->setFftFill(enable);
     uiDockProbe->setFftFill(enable);
+    triggerIQFftRedraw();
 }
 
 void MainWindow::setFftPeakHold(bool enable)
