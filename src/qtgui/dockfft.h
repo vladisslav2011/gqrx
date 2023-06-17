5fb17aa4d563ea346fc042e1c573235249e99adb
Vladslav P <vladisslav2011@gmail.com>
2023-06-17 20:38:54 +0300

    ui_builder: migrate dockfft

--- src/qtgui/dockfft.h
+++ src/qtgui/dockfft.h
@@ -24,7 +24,6 @@
 #define DOCKFFT_H
 
 #include <QDockWidget>
-#include <QSettings>
 #include "applications/gqrx/dcontrols_ui.h"
 
 namespace Ui {
@@ -40,77 +39,21 @@ public:
     explicit DockFft(QWidget *parent = 0);
     ~DockFft();
 
-    int fftRate();
-    int setFftRate(int fft_rate);
-
-    int fftSize();
-    int setFftSize(int fft_size);
 
     void setSampleRate(float sample_rate);
     void setFftLag(bool);
 
-    void saveSettings(QSettings *settings);
-    void readSettings(QSettings *settings);
+    void finalize() override;
+    void updateInfoLabels(int,int);
 
 signals:
-    void fftSizeChanged(int size);                 /*! FFT size changed. */
-    void fftRateChanged(int fps);                  /*! FFT rate changed. */
-    void fftWindowChanged(int window, int correction);             /*! FFT window type changed */
-    void wfSpanChanged(quint64 span_ms);           /*! Waterfall span changed. */
-    void fftSplitChanged(int pct);                 /*! Split between pandapter and waterfall changed. */
-    void fftZoomChanged(float level);              /*! Zoom level slider changed. */
-    void fftAvgChanged(float gain);                /*! FFT video filter gain has changed. */
-    void pandapterRangeChanged(float min, float max);
-    void waterfallRangeChanged(float min, float max);
-    void resetFftZoom(void);                       /*! FFT zoom reset. */
-    void gotoFftCenter(void);                      /*! Go to FFT center. */
-    void gotoDemodFreq(void);                      /*! Center FFT around demodulator frequency. */
-    void fftColorChanged(const QColor &);          /*! FFT color has changed. */
-    void fftFillToggled(bool fill);                /*! Toggle filling area under FFT plot. */
-    void fftPeakHoldToggled(bool enable);          /*! Toggle peak hold in FFT area. */
-    void peakDetectionToggled(bool enabled);       /*! Enable peak detection in FFT plot */
-    void bandPlanChanged(bool enabled);            /*! Toggle Band Plan at bottom of FFT area. */
-    void wfColormapChanged(const QString &cmap);
-    void wfThreadsChanged(const int n);
-
-public slots:
-    void setPandapterRange(float min, float max);
-    void setWaterfallRange(float min, float max);
-    void setWfResolution(quint64 msec_per_line);
-    void setZoomLevel(float level);
 
 private slots:
-    void on_fftSizeComboBox_currentIndexChanged(int index);
-    void on_fftRateComboBox_currentIndexChanged(int index);
-    void on_fftWinComboBox_currentIndexChanged(int index);
-    void on_windowCorrectionComboBox_currentIndexChanged(int index);
-    void on_wfSpanComboBox_currentIndexChanged(int index);
-    void on_fftSplitSlider_valueChanged(int value);
-    void on_fftAvgSlider_valueChanged(int value);
-    void on_fftZoomSlider_valueChanged(int level);
-    void on_pandRangeSlider_valuesChanged(int min, int max);
-    void on_wfRangeSlider_valuesChanged(int min, int max);
-    void on_resetButton_clicked(void);
-    void on_centerButton_clicked(void);
-    void on_demodButton_clicked(void);
-    void on_colorPicker_colorChanged(const QColor &);
-    void on_fillButton_toggled(bool checked);
-    void on_peakHoldButton_toggled(bool checked);
-    void on_peakDetectionButton_toggled(bool checked);
-    void on_lockButton_toggled(bool checked);
-    void on_bandPlanCheckbox_stateChanged(int state);
-    void on_cmapComboBox_currentIndexChanged(int index);
-    void on_threadsComboBox_currentTextChanged(const QString &text);
 
-private:
-    void updateInfoLabels(void);
 
 private:
     Ui::DockFft   * ui;
-//    float         m_maximumFftDb;
-//    float         m_minimumFftDb;
     float         m_sample_rate;
-    bool          m_pand_last_modified; /* Flag to indicate which slider was changed last */
 };
 
 #endif // DOCKFFT_H
