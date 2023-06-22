e63b91950e6fcda985c9506b1112be971b44384a
Vladslav P <vladisslav2011@gmail.com>
2023-06-22 04:33:18 +0300

    RDS: add WFM mono RAW mode

--- src/receivers/vfo.cpp
+++ src/receivers/vfo.cpp
@@ -305,6 +305,12 @@ bool vfo_s::set_wfm_deemph(const c_def::v_union & v)
     return true;
 }
 
+bool vfo_s::set_wfm_raw(const c_def::v_union & v)
+{
+    d_wfm_raw = v;
+    return true;
+}
+
 bool vfo_s::set_nb1_on(const c_def::v_union & v)
 {
     d_nb_on[0] = v;
@@ -475,6 +481,7 @@ void vfo_s::restore_settings(vfo_s& from, bool force)
     from.get_am_dcr(v);set_am_dcr(v);
     from.get_cw_offset(v);set_cw_offset(v);
     from.get_wfm_deemph(v);set_wfm_deemph(v);
+    from.get_wfm_raw(v);set_wfm_raw(v);
     from.get_rds_on(v);set_rds_on(v);
     from.get_test(v);set_test(v);
 }
@@ -631,6 +638,8 @@ int vfo_s::conf_initializer()
     getters[C_WFM_STEREO_DEEMPH]=&vfo_s::get_wfm_deemph;
     setters[C_WFM_OIRT_DEEMPH]=&vfo_s::set_wfm_deemph;
     getters[C_WFM_OIRT_DEEMPH]=&vfo_s::get_wfm_deemph;
+    setters[C_WFM_RAW]=&vfo_s::set_wfm_raw;
+    getters[C_WFM_RAW]=&vfo_s::get_wfm_raw;
 
     getters[C_RDS_ON]=&vfo_s::get_rds_on;
     setters[C_RDS_ON]=&vfo_s::set_rds_on;
