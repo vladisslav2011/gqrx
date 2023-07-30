b0e49a55ef957b0eb04bbd6aef7b2607f5fabf89
Vladslav P <vladisslav2011@gmail.com>
2023-07-30 04:23:00 +0300

    iq_formats: accelerated 12bit and 14bit formats

--- src/applications/gqrx/receiver.h
+++ src/applications/gqrx/receiver.h
@@ -303,8 +303,8 @@ private:
         any_to_any<gr_complex,std::complex<uint16_t>>::make(),
         any_to_any<gr_complex,std::complex<uint32_t>>::make(),
         any_to_any<gr_complex,std::array<int8_t,40>>::make(),
-        any_to_any<gr_complex,std::array<int8_t,3>>::make(),
-        any_to_any<gr_complex,std::array<int8_t,7>>::make(),
+        any_to_any<gr_complex,std::array<int8_t,24>>::make(),
+        any_to_any<gr_complex,std::array<int8_t,56>>::make(),
         nullptr
     };
     std::vector<any_to_any_base::sptr> convert_from
@@ -319,8 +319,8 @@ private:
         any_to_any<std::complex<uint16_t>,gr_complex>::make(),
         any_to_any<std::complex<uint32_t>,gr_complex>::make(),
         any_to_any<std::array<int8_t,40>,gr_complex>::make(),
-        any_to_any<std::array<int8_t,3>,gr_complex>::make(),
-        any_to_any<std::array<int8_t,7>,gr_complex>::make(),
+        any_to_any<std::array<int8_t,24>,gr_complex>::make(),
+        any_to_any<std::array<int8_t,56>,gr_complex>::make(),
         nullptr
     };
 
