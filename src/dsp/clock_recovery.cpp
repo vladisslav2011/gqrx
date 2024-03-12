625053871134564e954c694f9eaefe768532c204
Vladslav P <vladisslav2011@gmail.com>
2024-03-13 01:40:34 +0300

    RDS: avoid locking to weak correlation peak

--- src/dsp/clock_recovery.cpp
+++ src/dsp/clock_recovery.cpp
@@ -88,38 +88,69 @@ int clock_recovery_el_cc::general_work(int noutput_items,
     assert(d_mu <= 1.0);
 
     float mm_val = 0;
-    constexpr float dllbw=0.4f;
-    constexpr float dllalfa=0.2f;
+    constexpr float corr_alfa=0.001f;
 
     while (oo < noutput_items && ii < ni)
     {
-        float e_mu0=d_mu-dllbw;
-        float l_mu0=d_mu+dllbw;
+        float e_mu0=d_mu-d_dllbw;
+        float l_mu0=d_mu+d_dllbw;
         float e_mu90=d_omega*.5f+e_mu0;
         float l_mu90=d_omega*.5f+l_mu0;
-        int ei0=1+(int)floor(e_mu0);
-        int li0=1+(int)floor(l_mu0);
+        float e_mu180=d_omega+e_mu0;
+        float l_mu180=d_omega+l_mu0;
+        float e_mu270=d_omega*1.5f+e_mu0;
+        float l_mu270=d_omega*1.5f+l_mu0;
+
+        int ei0=ii+(int)floor(e_mu0);
+        int li0=ii+(int)floor(l_mu0);
         l_mu0 -= floor(l_mu0);
         e_mu0 -= floor(e_mu0);
-        int ei90=1+(int)floor(e_mu90);
-        int li90=1+(int)floor(l_mu90);
+        int ei90=ii+(int)floor(e_mu90);
+        int li90=ii+(int)floor(l_mu90);
         l_mu90 -= floor(l_mu90);
         e_mu90 -= floor(e_mu90);
-        d_e0acc+=(mag2(d_interp.interpolate(&in[ei0], e_mu0))-d_e0acc)*dllalfa;
-        d_e90acc+=(mag2(d_interp.interpolate(&in[ei90], e_mu90))-d_e90acc)*dllalfa;
-        d_l0acc+=(mag2(d_interp.interpolate(&in[li0], l_mu0))-d_l0acc)*dllalfa;
-        d_l90acc+=(mag2(d_interp.interpolate(&in[li90], l_mu90))-d_l90acc)*dllalfa;
-        mm_val=(d_l0acc-d_l90acc)-(d_e0acc-d_e90acc);
-        out[oo++] = d_interp.interpolate(&in[ii], d_mu);
+        int ei180=ii+(int)floor(e_mu180);
+        int li180=ii+(int)floor(l_mu180);
+        l_mu180 -= floor(l_mu180);
+        e_mu180 -= floor(e_mu180);
+        int ei270=ii+(int)floor(e_mu270);
+        int li270=ii+(int)floor(l_mu270);
+        l_mu270 -= floor(l_mu270);
+        e_mu270 -= floor(e_mu270);
+        if(d_skip)
+        {
+            d_skip=0;
+            mm_val=0.f;
+        }else{
+            d_e0acc+=(mag2(d_interp.interpolate(&in[ei0], e_mu0))-d_e0acc)*d_dllalfa;
+            d_e90acc+=(mag2(d_interp.interpolate(&in[ei90], e_mu90))-d_e90acc)*d_dllalfa;
+            d_l0acc+=(mag2(d_interp.interpolate(&in[li0], l_mu0))-d_l0acc)*d_dllalfa;
+            d_l90acc+=(mag2(d_interp.interpolate(&in[li90], l_mu90))-d_l90acc)*d_dllalfa;
+
+            d_e180acc+=(mag2(d_interp.interpolate(&in[ei180], e_mu180))-d_e180acc)*d_dllalfa;
+            d_e270acc+=(mag2(d_interp.interpolate(&in[ei270], e_mu270))-d_e270acc)*d_dllalfa;
+            d_l180acc+=(mag2(d_interp.interpolate(&in[li180], l_mu180))-d_l180acc)*d_dllalfa;
+            d_l270acc+=(mag2(d_interp.interpolate(&in[li270], l_mu270))-d_l270acc)*d_dllalfa;
+            d_corr0+=((d_l0acc-d_l90acc)+(d_e0acc-d_e90acc)-d_corr0)*corr_alfa;
+            d_corr180+=((d_l180acc-d_l270acc)+(d_e180acc-d_e270acc)-d_corr180)*corr_alfa;
+            if(d_corr0>=d_corr180)
+                mm_val=(d_l0acc-d_l90acc)-(d_e0acc-d_e90acc);
+            else
+                mm_val=(d_l180acc-d_l270acc)-(d_e180acc-d_e270acc);
+            d_skip=1;
+        }
+        if(d_corr0>=d_corr180)
+            out[oo++] = d_interp.interpolate(&in[ii], d_mu);
 
         d_omega = d_omega + d_gain_omega * mm_val;
         d_omega =
             d_omega_mid + gr::branchless_clip(d_omega - d_omega_mid, d_omega_lim);
 
-        mm_val = gr::branchless_clip(mm_val, d_omega*0.25f);
-        d_mu = d_mu + d_omega + d_gain_mu * mm_val;
+        d_mu = d_mu + d_omega + gr::branchless_clip(d_gain_mu * mm_val, d_omega*0.25f);
         ii += (int)floor(d_mu);
         d_mu -= floor(d_mu);
+        if(d_corr0<d_corr180)
+            out[oo++] = d_interp.interpolate(&in[ii], d_mu);
 
         if (ii < 1) // clamp it.  This should only happen with bogus input
             ii = 1;
