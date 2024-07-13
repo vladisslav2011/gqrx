95373a20a531998ba04623ef5542b4c1e544395a
Vladslav P <vladisslav2011@gmail.com>
2024-07-14 01:43:43 +0300

    Demod capability flags

--- src/applications/gqrx/receiver.cpp
+++ src/applications/gqrx/receiver.cpp
@@ -1288,9 +1288,8 @@ receiver::status receiver::set_demod_locked(Modulations::idx demod, int old_idx)
         // RX demod chain
         if (old_idx == -1)
         {
-            if (old_rxc != rxc)
-                if (old_rxc == Modulations::RX_CHAIN_WFMRX)
-                    set_value(C_RDS_ON, false);
+            if (Modulations::has_RDS(old_demod) && !Modulations::has_RDS(demod))
+                set_value(C_RDS_ON, false);
             background_rx();
             disconnect_rx();
         }
@@ -1316,7 +1315,7 @@ receiver::status receiver::set_demod_locked(Modulations::idx demod, int old_idx)
                 if(bool(tmp))
                     rx[d_current]->continue_audio_recording(old_rx);
             }
-            if (demod == Modulations::MODE_OFF)
+            if (!Modulations::has_audio(demod))
             {
                 c_def::v_union tmp;
                 rx[d_current]->get_audio_rec(tmp);
@@ -1827,7 +1826,7 @@ void receiver::connect_rx(int n)
         return;
     std::cerr<<"connect_rx "<<n<<" active "<<d_active<<" demod "<<rx[n]->get_demod()<<std::endl;
     rx[n]->set_timestamp_source(&d_iq_ts);
-    if (rx[n]->get_demod() != Modulations::MODE_OFF)
+    if (Modulations::has_audio(rx[n]->get_demod()))
     {
         if (d_active == 0)
         {
@@ -1876,7 +1875,7 @@ void receiver::disconnect_rx()
 void receiver::disconnect_rx(int n)
 {
     std::cerr<<"disconnect_rx "<<n<<" active "<<d_active<<" demod "<<rx[n]->get_demod()<<std::endl;
-    if (rx[n]->get_demod() != Modulations::MODE_OFF)
+    if (Modulations::has_audio(rx[n]->get_demod()))
     {
         d_active--;
         if (d_active == 0)
@@ -1940,7 +1939,7 @@ void receiver::disconnect_rx(int n)
 void receiver::background_rx()
 {
     std::cerr<<"background_rx "<<d_current<<" "<<rx[d_current]->get_demod()<<std::endl;
-    if (rx[d_current]->get_demod() != Modulations::MODE_OFF)
+    if (Modulations::has_audio(rx[d_current]->get_demod()))
     {
         tb->disconnect(rx[d_current], 0, audio_fft, 0);
         if (d_sniffer_active)
@@ -1954,7 +1953,7 @@ void receiver::background_rx()
 void receiver::foreground_rx()
 {
     std::cerr<<"foreground_rx "<<d_current<<" "<<rx[d_current]->get_demod()<<std::endl;
-    if (rx[d_current]->get_demod() != Modulations::MODE_OFF)
+    if (Modulations::has_audio(rx[d_current]->get_demod()))
     {
         tb->connect(rx[d_current], 0, audio_fft, 0);
         if (d_sniffer_active)
--- src/receivers/modulations.h
+++ src/receivers/modulations.h
@@ -113,6 +113,12 @@ public:
         int click_res;
         int fft_lo;
         int fft_hi;
+        bool audio:1; /* special and data-only demodulators do not produce an audio */
+        bool RDS:1; /* WFM family */
+        bool data:1; /* TODO: CW, FT8/FT4, MPSK, Olivia, etc.*/
+        bool structured_data:1; /* TODO: RDS PI/PS logging */
+        bool picture:1; /* TODO: SSTV, FAX */
+        bool video:1; /* TODO: ATV, FPV */
         filter_presets presets;
         freq_ranges ranges;
     };
@@ -121,78 +127,91 @@ public:
         {GRP_OFF,"!","Demod Off",
             10,
             -24000,24000,
+            0,0,0,0,0,0,
             {{      0,      0}, {     0,     0}, {     0,     0}},  // MODE_OFF
             {{      0,      0}, {     0,     0}},  // MODE_OFF
         },
         {GRP_RAW,"I","Raw I/Q",
             100,
             -24000,24000,
+            1,0,0,0,0,0,
             {{ -15000,  15000}, { -5000,  5000}, { -1000,  1000}},  // MODE_RAW
             {{ -40000,   -200}, {   200, 40000}},  // MODE_RAW
         },
         {GRP_AM,"A","AM",
             100,
             0,6000,
+            1,0,0,0,0,0,
             {{ -10000,  10000}, { -5000,  5000}, { -2500,  2500}},  // MODE_AM
             {{ -40000,   -200}, {   200, 40000}},  // MODE_AM
         },
         {GRP_AM_SYNC,"Shift+A","AM-Sync",
             100,
             0,6000,
+            1,0,0,0,0,0,
             {{ -10000,  10000}, { -5000,  5000}, { -2500,  2500}},  // MODE_AMSYNC
             {{ -40000,   -200}, {   200, 40000}},  // MODE_AMSYNC
         },
         {GRP_SSB,"S","LSB",
             100,
             0,3000,
+            1,0,0,0,0,0,
             {{  -4000,   -100}, { -2800,  -100}, { -2400,  -300}},  // MODE_LSB
             {{ -40000,   -100}, { -5000,     0}},  // MODE_LSB
         },
         {GRP_SSB,"Shift+S","USB",
             100,
             0,3000,
+            1,0,0,0,0,0,
             {{    100,   4000}, {   100,  2800}, {   300,  2400}},  // MODE_USB
             {{      0,   5000}, {   100, 40000}},  // MODE_USB
         },
         {GRP_CW,"C","CW-L",
             10,
             0,1500,
+            1,0,0,0,0,0,
             {{  -1000,   1000}, {  -250,   250}, {  -100,   100}},  // MODE_CWL
             {{  -5000,   -100}, {   100,  5000}},  // MODE_CWL
         },
         {GRP_CW,"Shift+C","CW-U",
             10,
             0,1500,
+            1,0,0,0,0,0,
             {{  -1000,   1000}, {  -250,   250}, {  -100,   100}},  // MODE_CWU
             {{  -5000,   -100}, {   100,  5000}},  // MODE_CWU
         },
         {GRP_NFM,"N","Narrow FM",
             100,
             0,5000,
+            1,0,0,0,0,0,
             {{ -10000,  10000}, { -5000,  5000}, { -2500,  2500}},  // MODE_NFM
             {{ -40000,   -200}, {   200, 40000}},  // MODE_NFM
         },
         {GRP_NFMPLL,"Shift+N","NFM PLL",
             100,
             0,5000,
+            1,0,0,0,0,0,
             {{ -10000,  10000}, { -5000,  5000}, { -2500,  2500}},  // MODE_NFMPLL
             {{ -40000,   -200}, {   200, 40000}},  // MODE_NFMPLL
         },
         {GRP_WFM_MONO,"W","WFM (mono)",
             1000,
             0,24000,
+            1,1,0,0,0,0,
             {{-100000, 100000}, {-80000, 80000}, {-60000, 60000}},  // MODE_WFM_MONO
             {{-120000, -10000}, { 10000,120000}},  // MODE_WFM_MONO
         },
         {GRP_WFM_STEREO,"Shift+W","WFM (stereo)",
             1000,
             0,24000,
+            1,1,0,0,0,0,
             {{-100000, 100000}, {-80000, 80000}, {-60000, 60000}},  // MODE_WFM_STEREO
             {{-120000, -10000}, { 10000,120000}},  // MODE_WFM_STEREO
         },
         {GRP_WFM_STEREO_OIRT,"O","WFM (oirt)",
             1000,
             0,24000,
+            1,1,0,0,0,0,
             {{-100000, 100000}, {-80000, 80000}, {-60000, 60000}},  // MODE_WFM_STEREO_OIRT
             {{-120000, -10000}, { 10000,120000}},  // MODE_WFM_STEREO_OIRT
         },
@@ -201,6 +220,35 @@ public:
     static constexpr const char * GetStringForModulationIndex(int iModulationIndex)
     {
         return modes[iModulationIndex].name;
+    }
+    template<typename T> static constexpr int sanitize_mode(T iModulationIndex)
+    {
+        return (int(iModulationIndex)<0)?0:
+            (int(iModulationIndex)>=MODE_COUNT)?0:int(iModulationIndex);
+    }
+    template<typename T> static constexpr bool has_audio(T iModulationIndex)
+    {
+        return modes[sanitize_mode(iModulationIndex)].audio;
+    }
+    template<typename T> static constexpr bool has_RDS(T iModulationIndex)
+    {
+        return modes[sanitize_mode(iModulationIndex)].RDS;
+    }
+    template<typename T> static constexpr bool has_data(T iModulationIndex)
+    {
+        return modes[sanitize_mode(iModulationIndex)].data;
+    }
+    template<typename T> static constexpr bool has_structured_data(T iModulationIndex)
+    {
+        return modes[sanitize_mode(iModulationIndex)].structured_data;
+    }
+    template<typename T> static constexpr bool has_picture(T iModulationIndex)
+    {
+        return modes[sanitize_mode(iModulationIndex)].picture;
+    }
+    template<typename T> static constexpr bool has_video(T iModulationIndex)
+    {
+        return modes[sanitize_mode(iModulationIndex)].video;
     }
     static bool IsModulationValid(QString strModulation);
     static idx GetEnumForModulationString(QString param);
