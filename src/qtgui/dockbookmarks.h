1e43427705eddce6a02550e6856c37e6966e58d8
Vladslav P <vladisslav2011@gmail.com>
2024-11-10 03:33:52 +0300

    bookmarks: Avoid leaking QDialog on each show

--- src/qtgui/dockbookmarks.h
+++ src/qtgui/dockbookmarks.h
@@ -25,6 +25,7 @@
 #include <QDockWidget>
 #include <QTableWidgetItem>
 #include "qtgui/bookmarkstablemodel.h"
+#include "qtgui/bookmarkstaglist.h"
 #include <QItemDelegate>
 #include <QSettings>
 #include "receivers/defines.h"
@@ -51,6 +52,8 @@ class DockBookmarks : public QDockWidget
 private:
     Ui::DockBookmarks *ui;
     QMenu*             contextmenu;
+    QDialog*           tagsDialog;
+    BookmarksTagList*  dialogTaglist;
     qint64             m_currentFrequency;
     bool               m_updating;
     BookmarksTableModel *bookmarksTableModel;
